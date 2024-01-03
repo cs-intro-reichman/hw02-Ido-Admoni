28671e1f86ff7df3ac06ed23753a0b0508b59004
Ido Admoni <ido.admoni@post.runi.ac.il>
2024-01-03 17:03:26 +0200

    HW2 done!

--- Perfect.java
+++ Perfect.java
@@ -2,23 +2,28 @@
  *  Gets a command-line argument (int), and chekcs if the given number is perfect.
  */
 public class Perfect {
-	public static void main (String[] args) {
-	int num = Integer. parseInt(args[0]);
-	int sum = 0;
-	int since = 0;
-	String why = "";
-	for (int i = 1; i < num; i++){
-			if (num%i == 0) {
-				sum = sum + i;
-				why = why + " + " + i;
-			}
-}
-	if (sum == num) {
-		System.out.println(num +" is a perfect number since " + num + " = " + why);}	
-	else {
-		System.out.println(num + " is not a perfect number");
-			}
-	}
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        int sum = 0;
+        String why = "";
+
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum = sum + i;
+                if (why.isEmpty()) {
+                    why = why + i;
+                } else {
+                    why = why + " + " + i;
+                }
+            }
+        }
+
+        if (sum == num) {
+            System.out.println(num + " is a perfect number since " + num + " = " + why);
+        } else {
+            System.out.println(num + " is not a perfect number");
+        }
+    }
 }
 
 	
